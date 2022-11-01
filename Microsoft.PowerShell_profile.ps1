e31aaf12ceeee6e3da274f7111eab2019b3f6109
hanzsalat <HanzSalat@gmx.de>
2022-11-01 14:30:44 +0100

    modified:   Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 # Init oh-my-posh with profile from git
 oh-my-posh init pwsh --config 'C:\Users\Simon\Documents\GitHub\Oh-My-Posh-Profile\Custom-Profile.omp.json' | Invoke-Expression
 # custom fucntions to speed up workflow
-function Find-ChildItem{
+function Find-ChildItem {
     Param (
         [Parameter(Position=1)]
         [string]$name
@@ -11,3 +11,8 @@ function Find-ChildItem{
         Get-ChildItem | Where-Object {$_.Name -match "$name"}
     }
 }
+function Get-ItemCount {
+    (Get-ChildItem).Count
+}
+New-Alias -Name 'fci' -Value "Find-ChildItem"
+New-Alias -Name 'gic' -Value "Get-ItemCount" 
