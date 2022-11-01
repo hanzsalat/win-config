585cac5e4c96bfa954239e6b1fde79134a5f0be3
hanzsalat <HanzSalat@gmx.de>
2022-11-01 14:20:07 +0100

    new file:   Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+# Init oh-my-posh with profile from git
+oh-my-posh init pwsh --config 'C:\Users\Simon\Documents\GitHub\Oh-My-Posh-Profile\Custom-Profile.omp.json' | Invoke-Expression
+# custom fucntions to speed up workflow
+function Find-ChildItem{
+    Param (
+        [Parameter(Position=1)]
+        [string]$name
+    )
+
+    process{
+        Get-ChildItem | Where-Object {$_.Name -match "$name"}
+    }
+}
