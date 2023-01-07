bea47743e3ffb5b3abda998b98facf7686924f0e
hanzsalat <HanzSalat@gmx.de>
2023-01-07 21:34:09 +0100

    modified:   profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,27 +1,25 @@
 # Init OMP with config
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/lambda.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\zash.omp.json" | Invoke-Expression
 
-# Import Packwiz auto completion
+# Import modules
+Get-InstalledModule |
+ForEach-Object { Import-Module $_.name }
+Import-Module Random-Utils
+
+# Import auto completions
 Import-Module PackwizCompletion
-
-# enable completion in current shell, use absolute path because PowerShell Core not respect $env:PSModulePath
 Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
-
-# Choco completion for powershell
-Import-Module “$env:ChocolateyInstall\helpers\chocolateyProfile.psm1” -Force
+Import-Module “$env:ChocolateyInstall\helpers\chocolateyProfile.psm1”
 
 # Quick access to navigate on system
-$downloads = $home + '\Downloads'
-$desktop = $home + '\Desktop'
-$documents = $home + '\Documents'
+$dwnld = $home + '\Downloads'
+$dskt = $home + '\Desktop'
+$doc = $home + '\Documents'
 $git = $home + '\Documents\GitHub'
 $mc = $env:SystemDrive + '\Games\Games 2\Minecraft'
 
 # Add/Change Aliases
-Set-Alias -Name 'omp' -Value "oh-my-posh.exe" -Description 'Oh-My-Posh short alias'
-Set-Alias -Name 'pw' -Value "packwiz.exe" -Description 'Packwiz short alias'
+Set-Alias -Name 'omp' -Value "oh-my-posh" -Description 'Oh-My-Posh short alias'
+Set-Alias -Name 'pw' -Value "packwiz" -Description 'Packwiz short alias'
 Set-Alias -Name 'gwu' -Value "Get-WindowsUpdate" -Description 'Get-WindowsUpdate short alias'
 Set-Alias -Name 'iwu' -Value "Install-WindowsUpdate" -Description 'Install-WindowsUpdate short alias'
-Set-Alias -Name 'gic'  "Get-ItemCount" -Description 'Get-ItemCount short alias'
-Set-Alias -Name 'fci' -Value "Find-ChildItem" -Description 'Find-ChildItem short alias'
-Set-Alias -Name 'scmd' -Value "Search-Command" -Description 'Search-Command short alias'
