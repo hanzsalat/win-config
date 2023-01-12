9af09770008de03930983a124025fb536c4fb314
hanzsalat <HanzSalat@gmx.de>
2023-01-13 00:52:20 +0100

    modified:   profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,10 +1,13 @@
 # Check for installed software
 $check = @{
     Scoop = $($env:Path.Split(';').where({$_ -match 'scoop'}))
+    ScoopCompletion = "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
     Posh = $($env:Path.Split(';').where({$_ -match 'oh-my-posh'})) + '\oh-my-posh.exe'
     Choco = $env:ChocolateyInstall + '\choco.exe'
     Packwiz = $env:GOPATH + '\bin\packwiz.exe'
+    PackwizCompletion = $env:PSModulePath.split(';') | Get-ChildItem -Directory -ErrorAction SilentlyContinue | Where-Object {$_ -match 'Packwiz'}
     SpotifyTui = $($check.Scoop + '\spt.exe')
+    SpotifyTuiCompletion = $env:PSModulePath.split(';') | Get-ChildItem -Directory -ErrorAction SilentlyContinue | Where-Object {$_ -match 'Spotify'}
 }
 
 $checked = @{}
@@ -20,16 +23,16 @@ foreach ($module in $modules.Name) { Import-Module $module }
 
 # Set dependecies after check
 if ($checked.Posh) { 
-    oh-my-posh init pwsh --config "C:\Users\simon\Documents\PowerShell\Themes\zash_V2.omp.json" | Invoke-Expression 
+    oh-my-posh init pwsh --config "$env:USERPROFILE\Documents\Github\Pwsh\zash_V2.omp.json" | Invoke-Expression 
     Set-Alias -Name 'omp' -Value "oh-my-posh" -Description 'Oh-My-Posh short alias'
 }
-if ($checked.Scoop) { Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion" }
+if ($checked.ScoopCompletion -and $checked.Scoop) { Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion" }
 if ($checked.Choco) { Import-Module “$env:ChocolateyInstall\helpers\chocolateyProfile.psm1” }
-if ($checked.Packwiz) { 
+if ($checked.PackwizCompletion -and $checked.Packwiz) { 
     Import-Module PackwizCompletion 
     Set-Alias -Name 'pw' -Value "packwiz" -Description 'Packwiz short alias'
 }
-if ($checked.SpotifyTui) { Import-Module SpotifyTuiCompletion }
+if ($checked.SpotifyTuiCompletion -and $checked.SpotifyTui) { Import-Module SpotifyTuiCompletion }
 
 # Quick access to navigate on system
 $dwnld = $home + '\Downloads'
