35b07b5b46d09bd224abf1ff920aad861ceb95fe
hanzsalat <HanzSalat@gmx.de>
2023-01-14 02:49:06 +0100

    modified:   profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,38 +1,37 @@
 # Check for installed software
 $check = @{
-    Scoop = (Get-Command scoop).Path
+    Scoop = "$($(Get-Command scoop).Path)"
     ScoopCompletion = "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
-    Posh = (Get-Command oh-my-posh).Path
-    Choco = (Get-Command choco).Path
-    Packwiz = (Get-Command packwiz).Path
-    PackwizCompletion = (Get-Module -ListAvailable -Name PackwizCompletion).Path
-    SpotifyTui = (Get-Command spt).Path
-    SpotifyTuiCompletion = (Get-Module -ListAvailable -Name SpotifyTuiCompletion).Path
+    Posh = "$($(Get-Command oh-my-posh).Path)"
+    Choco = "$($(Get-Command choco).Path)"
+    Packwiz = "$($(Get-Command packwiz).Path)"
+    PackwizCompletion = "$($(Get-Module -ListAvailable -Name PackwizCompletion).Path)"
+    SpotifyTui = "$($(Get-Command spt).Path)"
+    SpotifyTuiCompletion = "$($(Get-Module -ListAvailable -Name SpotifyTuiCompletion).Path)"
 }
 
 $checked = @{}
 
 foreach ($content in $check.GetEnumerator()) {
-    if (Test-Path $content.Value) { $checked.Add($content.Key,$true) }
+    if ([System.IO.File]::Exists($content.Value)) { $checked.Add($content.Key,$true) }
     else { $checked.Add($content.Key,$false) }
 }
 
 # Import modules
-#$modules = Get-InstalledModule
-#foreach ($module in $modules.Name) { Import-Module $module }
+
 
 # Set dependecies after check
 if ($checked.Posh) { 
     oh-my-posh init pwsh --config "$env:USERPROFILE\Documents\Github\Pwsh\zash_V2.omp.json" | Invoke-Expression 
     Set-Alias -Name 'omp' -Value "oh-my-posh" -Description 'Oh-My-Posh short alias'
 }
-if ($checked.ScoopCompletion -and $checked.Scoop) { Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion" }
+if ($checked.ScoopCompletion) { Import-Module scoop-completion }
 if ($checked.Choco) { Import-Module “$env:ChocolateyInstall\helpers\chocolateyProfile.psm1” }
-if ($checked.PackwizCompletion -and $checked.Packwiz) { 
+if ($checked.PackwizCompletion) { 
     Import-Module PackwizCompletion 
     Set-Alias -Name 'pw' -Value "packwiz" -Description 'Packwiz short alias'
 }
-if ($checked.SpotifyTuiCompletion -and $checked.SpotifyTui) { Import-Module SpotifyTuiCompletion }
+if ($checked.SpotifyTuiCompletion) { Import-Module SpotifyTuiCompletion }
 
 # Quick access to navigate on system
 $dwnld = $home + '\Downloads'
