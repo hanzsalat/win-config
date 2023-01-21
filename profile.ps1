99aba12cdcb986f477eacb24bf7000e8063e64c6
hanzsalat <HanzSalat@gmx.de>
2023-01-21 17:26:01 +0100

    modified:   profile.ps1 	modified:   zash_V2.omp.json

--- profile.ps1
+++ profile.ps1
@@ -1,16 +1,15 @@
 # Generate list of Items that need to be checked
 $check = @{}
-# Sofware
 [void]$check.Add('Choco', (Get-Command choco -ErrorAction SilentlyContinue).Path)
 [void]$check.Add('Packwiz', (Get-Command packwiz -ErrorAction SilentlyContinue).Path)
 [void]$check.Add('Posh', (Get-Command oh-my-posh -ErrorAction Ignore).Path)
 [void]$check.Add('PSReadLine', ((Get-Module -ListAvailable -Name PSReadLine).Path | Select-Object -First 1))
 [void]$check.Add('PSWindowsUpdate', ((Get-Module -ListAvailable -Name PSWindowsUpdate).Path | Select-Object -First 1))
 [void]$check.Add('RandomUtils', ((Get-Module -ListAvailable -Name Random-Utils).Path | Select-Object -First 1))
+[void]$check.Add('TerminalIcons', ((Get-Module -ListAvailable -Name Terminal-Icons).Path | Select-Object -First 1))
 [void]$check.Add('Scoop', (Get-Command scoop -ErrorAction SilentlyContinue).Path)
 [void]$check.Add('SpotifyTui', (Get-Command spt -ErrorAction SilentlyContinue).Path)
 #[void]$check.add('Starship', (Get-Command starship -ErrorAction SilentlyContinue).Path)
-# Completion
 [void]$check.Add('ChocoCompletion', "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1")
 [void]$check.Add('OpCompletion', ((Get-Module -ListAvailable -Name OpCompletion).Path | Select-Object -First 1))
 [void]$check.Add('PackwizCompletion', ((Get-Module -ListAvailable -Name PackwizCompletion).Path | Select-Object -First 1))
@@ -24,7 +23,6 @@ foreach ($content in $check.GetEnumerator()) {
     else { [void]$checked.Add($content.Key, $false) }
 }
 # Set dependecies after check
-# Software
 if ($checked.Packwiz) { Set-Alias -Name 'pw' -Value "packwiz" -Description 'Packwiz short alias' }
 if ($checked.Posh) {
     & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:USERPROFILE\Documents\Github\Pwsh\zash_V2.omp.json" --print) -join "`n"))
@@ -37,7 +35,7 @@ if ($checked.PSWindowsUpdate) {
     Set-Alias -Name 'gwu' -Value "Get-WindowsUpdate" -Description 'Get-WindowsUpdate short alias'
     Set-Alias -Name 'iwu' -Value "Install-WindowsUpdate" -Description 'Install-WindowsUpdate short alias'
 }
-# Completion
+if ($checked.TerminalIcons) { Import-Module Terminal-Icons }
 if ($checked.ChocoCompletion) { Import-Module $check.ChocoCompletion }
 if ($checked.OpCompletion) { Import-Module OpCompletion }
 if ($checked.PackwizCompletion) { Import-Module PackwizCompletion }
