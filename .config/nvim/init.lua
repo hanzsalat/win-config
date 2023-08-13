a0ed6277b72f612a623686c3632446e9f7943ced
hanzsalat <hanzsalat@gmx.de>
2023-08-13 17:27:28 +0200

    modified:   .config/nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -217,6 +217,9 @@ require('lazy').setup({
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 
+-- Set relative numbers
+vim.opt.rnu = true
+
 -- Set highlight on search
 vim.o.hlsearch = false
 
